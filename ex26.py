f88522de4ae8c9a59c2bd2dceeb140d319ec9395
jacyyang <jacyyang04@gmail.com>
2020-10-23 09:57:11 -0700

    completed exercise

--- ex26.py
+++ ex26.py
@@ -19,6 +19,7 @@ cities = {
 #adding cities to cities dictionary
 cities['WI'] = 'Green Bay'
 cities['WA'] = 'Everett'
+cities['RI'] = 'Providence'
 
 #print states
 print('-' * 20)
@@ -29,3 +30,34 @@ print("Colorado's abbreviation is: ", states['Colorado'])
 print('-' * 20)
 print('WI state has: ', cities['WI'])
 print('CA state has: ', cities['CA'])
+
+#using state then cities dictionary
+print('-' * 20)
+print('Washington has: ', cities[states['Washington']])
+print('Florida has: ', cities[states['Florida']])
+
+#print every state abbreviation
+print('-' * 20)
+for state, abbre in states.items(): #state is key, value is abbre
+    print('%s is abbreviated as %s.' % (state, abbre))
+    
+#print every cite in state
+print('-' * 20)
+for abbre, city in cities.items(): #abbre, city is the key-value pair
+    print('%s has the city %s.' % (abbre, city))
+    
+#doing both at the same time
+print('-' * 20)
+for state, abbre in states.items():
+    print('%s is abbreviated as %s and has %s.' % (state, abbre, cities[abbre]))
+
+print('-' * 20)    
+#abbreviating a state that may not be there
+state = states.get('Michigan', None)
+
+if not state:
+    print('Sorry, that state is not in the dictionary.')
+    
+#get a city with a default value
+city = cities.get('MI', 'not listed')
+print('The city for MI is: %s' %city)
