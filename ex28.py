b0831a49c5e873dd38b9d93bc490a3a4928cdd4e
jacyyang <jacyyang04@gmail.com>
2020-11-06 12:02:15 -0800

    added comments

--- ex28.py
+++ ex28.py
@@ -5,10 +5,12 @@ import random
 from urllib.request import urlopen
 import sys
 
+#create url variable
 WORD_URL = "http://learncodethehardway.org/words.txt"
-#create empty list
+#create empty list 
 WORDS = []
-#dictionary
+
+#create objects
 PHRASES = {
     "class %%%(%%%):":  "Make a class named %%% that is-a %%%.",
     "class %%%(object):\n\tdef __init__(self, %%%):":
@@ -20,7 +22,8 @@ PHRASES = {
     "***.*** = '***'": "From *** get the *** attribute and set it to '***'"
     }
     
-# do they want to drill phrases first
+
+#do they want to drill phrases first
 #set boolean to false
 PHRASE_FIRST = False
 if len(sys.argv) == 2 and sys.argv[1] == "english":
@@ -31,8 +34,7 @@ for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())
     
 def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in
-                   random.sample(WORDS, snippet.count("%%%"))]
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
