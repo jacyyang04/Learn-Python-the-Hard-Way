dbac519ec143898daa28cac55d215b04992a1934
jacyyang <jacyyang04@gmail.com>
2020-12-07 12:05:11 -0800

    adding Class Map

--- SpaceGame.py
+++ SpaceGame.py
@@ -1,1 +1,27 @@
 #Gothons from Planet Percel #25
+
+#class for Maps
+Class Map(object):
+
+    def __init__(self, starting_scene):
+        pass
+    
+    def next_scene(self, scene_name):
+        pass
+    
+    def opening_scene(self):
+        pass
+        
+Class Engine(object):
+    def __init__(self, scene_map):
+        pass
+        
+Scene:
+--> enter
+ - Death
+ - Central Corridor
+ - Weapons
+ - Naviagtion
+ - Bridge
+ - Escape Mod
+ - Medic
